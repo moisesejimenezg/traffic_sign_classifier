d96edbedc5427bc5bef1e8e1f8587dff11ca7566
Moises Jimenez <moisesejimenezg@outlook.com>
2020-11-16 21:27:30 +0100

    Add grid visualization of all the different signs

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def fill_empty_position(axes, x, y, img):
+    axes[x, y].imshow(np.zeros(img.squeeze().shape))
+    axes[x, y].get_xaxis().set_visible(False)
+    axes[x, y].get_yaxis().set_visible(False)
+
 
 class DataVisualizer:
     def __init__(self, data, labels, class_number):
@@ -11,8 +16,7 @@ class DataVisualizer:
     def visualize(self):
         self.__generate_histogram()
         positions = self.__find_examples()
-        for position in positions:
-            self.__display_image(position)
+        self.__display_grid(positions)
 
     def __find_examples(self):
         added = 0
@@ -26,11 +30,21 @@ class DataVisualizer:
             i += 1
         return positions
 
-    def __display_image(self, idx):
-        plt.imshow(self.data[idx].squeeze())
+    def __display_grid(self, positions):
+        rows = 9
+        cols = 5
+        i = 0
+        f, axes = plt.subplots(rows, cols)
+        for position in positions:
+            x = i // cols
+            y = i % cols
+            axes[x, y].imshow(self.data[position].squeeze())
+            axes[x, y].get_xaxis().set_visible(False)
+            axes[x, y].get_yaxis().set_visible(False)
+            i += 1
+        fill_empty_position(axes, 8, 3, self.data[0])
+        fill_empty_position(axes, 8, 4, self.data[0])
         plt.show()
 
     def __generate_histogram(self):
-        # histogram = np.histogram(self.labels, range(0, 43))
         plt.hist(self.labels, range(0, 43))
-        plt.show()
