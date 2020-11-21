5e2bb9465ce11cfc826c9fa2f1af7a8ad950e9c1
Moises Jimenez <moisesejimenezg@outlook.com>
2020-11-21 23:32:49 +0100

    Add image_cleaner to delete unnecessary images
    - add labels to pickle file

--- pickle_creator.py
+++ pickle_creator.py
@@ -4,13 +4,26 @@ from PIL import Image
 import numpy as np
 internet = {"labels":[], "images": [], "names": []}
 
-for file_name in listdir("extra_traffic_signs"):
+valid_images = open("valid_images.txt")
+valid_images = valid_images.read()
+valid_images = valid_images.split('\n')
+valid_images.sort()
+
+folder = 'extra_traffic_signs/'
+ground_truth = open('extra_traffic_signs/GT-final_test.csv')
+ground_truth = ground_truth.read()
+ground_truth = ground_truth.split('\n')
+ground_truth = [int(x.split(';')[7]) for x in ground_truth if x != '' and folder + x.split(';')[0] in valid_images]
+
+files = listdir("extra_traffic_signs")
+files.sort()
+for file_name in files:
     if ".ppm" in file_name:
         full_name = "extra_traffic_signs/" + file_name
         image = np.array(Image.open(full_name))
         if image.shape[0] == 32 and image.shape[1] == 32:
             internet["images"].append(image)
             internet["names"].append(full_name)
-            print(full_name)
+internet['labels'] = ground_truth
 
 pickle.dump(internet, open("internet.p", "wb"))
